52ef7763654379008c20affbc4dcaba79249266c
Great-rzh <2506895765@qq.com>
2020-02-27 22:13:06 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+	public static void main (String[] args){
+		System.out.println("hello world !");
+System.out.println("z !");
+System.out.println("sa");
+System.out.println("as");
+System.out.println("x");
+	}
+}
